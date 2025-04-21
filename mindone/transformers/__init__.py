f46d10967ecd3c6ce1499b306752aa491777a45e
litingyu <605979840@qq.com>
2025-04-21 15:47:13 +0800

    bugfix

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -112,6 +112,7 @@ from .models.umt5 import (
     UMT5Model,
     UMT5PreTrainedModel,
 )
+from .models.vits import VitsModel, VitsPreTrainedModel
 from .models.wav2vec2 import (
     Wav2Vec2FeatureExtractor,
     Wav2Vec2ForAudioFrameClassification,
@@ -124,6 +125,5 @@ from .models.wav2vec2 import (
     Wav2Vec2PreTrainedModel,
     Wav2Vec2Processor,
 )
-from .models.vits import VitsModel, VitsPreTrainedModel
 from .models.xlm_roberta import XLMRobertaModel, XLMRobertaPreTrainedModel
 from .processing_utils import ProcessorMixin
